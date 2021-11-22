587a2a1ce5e2abfa64a8b1bcf4bfb108235d6c5e
Eddie <eddielacrosse2@gmail.com>
2021-11-22 14:53:17 -0500

    Initial Project build with Controller structures

---
+++ Instagram/Controllers/Core Tabs/ExploreViewController.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+class ExploreViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
--- Instagram/Controllers/Other/SearchResultViewController.swift
+++ Instagram/Controllers/Other/SearchResultViewController.swift
@@ -1,19 +1,10 @@
-//
-//  ViewController.swift
-//  Instagram
-//
-//  Created by Eddie on 11/21/21.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
+class SearchResultViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
-
-
 }
-
